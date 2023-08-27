36de5882074cdde9dbf548932770f2d131b46797
aayushk22 <ayushkumar22.ak@gmail.com>
2023-08-27 22:17:04 +0530

    1st

--- src/main/java/com/driver/Order.java
+++ src/main/java/com/driver/Order.java
@@ -1,5 +1,7 @@
 package com.driver;
 
+import io.swagger.models.auth.In;
+
 public class Order {
 
     private String id;
@@ -9,7 +11,22 @@ public class Order {
 
         // The deliveryTime has to converted from string to int and then stored in the attribute
         //deliveryTime  = HH*60 + MM
+        this.id = id;
+        String hh = deliveryTime.substring(0,2);
+        String mm = deliveryTime.substring(3);
+        int h = Integer.parseInt(hh) * 60;
+        int m = Integer.parseInt(mm);
+        this.deliveryTime = h + m;
+//        System.out.println(this.deliveryTime);
     }
+
+//    public void setId(String id) {
+//        this.id = id;
+//    }
+//
+//    public void setDeliveryTime(int deliveryTime) {
+//        this.deliveryTime = deliveryTime;
+//    }
 
     public String getId() {
         return id;
